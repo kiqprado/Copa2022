816641c6746d40afbf6327c2fb95d4650a118412
kiqprado <kiqprado@outlook.com>
2022-12-07 12:25:03 -0300

    update cards

--- main.js
+++ main.js
@@ -30,42 +30,22 @@ function createCard(date, day, games) {
 
 document.querySelector('#cards').innerHTML =
   createCard(
-    '03/12',
+    '09/12',
+    'Sexta',
+    createGame('croatia', 'CRO', '12:00', 'brazil', 'BRA')
+  ) +
+  createCard(
+    '09/12',
+    'Sexta',
+    createGame('netherlands', 'HOL', '16:00', 'argentina', 'ARG')
+  ) +
+  createCard(
+    '10/12',
     'Sábado',
-    createGame('netherlands', 'HOL', '12:00', 'united-states', 'EUA')
+    createGame('morocco', 'MAR', '12:00', 'portugal', 'POR')
   ) +
   createCard(
-    '03/12',
+    '10/12',
     'Sábado',
-    createGame('argentina', 'ARG', '16:00', 'australia', 'AUS')
-  ) +
-  createCard(
-    '04/12',
-    'Domingo',
-    createGame('france', 'FRA', '12:00', 'poland', 'POL')
-  ) +
-  createCard(
-    '04/12',
-    'Domingo',
-    createGame('england', 'ING', '16:00', 'senegal', 'SEN')
-  ) +
-  createCard(
-    '05/12',
-    'Segunda',
-    createGame('japan', 'JAP', '12:00', 'croatia', 'CRO')
-  ) +
-  createCard(
-    '05/12',
-    'Segunda',
-    createGame('brazil', 'BRA', '16:00', 'south-korea', 'COR')
-  ) +
-  createCard(
-    '06/12',
-    'Terça',
-    createGame('morocco', 'MAR', '12:00', 'spain', 'ESP')
-  ) +
-  createCard(
-    '06/12',
-    'Terça',
-    createGame('portugal', 'POR', '16:00', 'switzerland', 'SUI')
-  )
+    createGame('england', 'ING', '16:00', 'france', 'FRA')
+  ) 
